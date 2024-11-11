6726a2b2573fad32b96622b9da374fa642ae785b
LastProgram <145275888+LastProgram@users.noreply.github.com>
2024-11-11 08:15:09 +0700

    Edit MainActivity

--- app/src/main/java/com/example/android_lab2/MainActivity.kt
+++ app/src/main/java/com/example/android_lab2/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.android_lab2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.util.Objects
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +17,23 @@ class MainActivity : AppCompatActivity() {
         val editValue = findViewById<EditText>(R.id.edit_value)
         val editConstrain = findViewById<EditText>(R.id.edit_constrain)
         val result = findViewById<TextView>(R.id.result)
+        val button = findViewById<Button>(R.id.button)
 
+        button.setOnClickListener {
+            if(isTextFloat(editValue.text) && isTextFloat(editConstrain.text))
+            {
+                result.text = "result"
+            }
+            else
+            {
+                result.text = "Заполните все поля!"
+            }
+        }
 
+    }
+
+    private fun isTextFloat(input: Editable) : Boolean
+    {
+        return input.toString().toFloatOrNull() != null
     }
 }
